9bdf95f186db852bcc2b4bcc05d0ca6b8c88e1aa
BS-CURTIS <curtis@buying-square.com>
2022-08-22 17:38:15 +0900

    env수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { GetStaticProps } from "next";
-import { Contatiner } from "../component/styles/styled";
-import { body } from "../interface/common";
+import React from 'react';
+import { GetStaticProps } from 'next';
+import { Contatiner } from '../component/styles/styled';
+import { body } from '../interface/common';
 
-import axios from "axios";
-import IndexContent from "../component/contents/indexContent";
+import axios from 'axios';
+import IndexContent from '../component/contents/indexContent';
 
 const Index = ({ body }: body) => {
   const { items } = body;
@@ -18,7 +18,7 @@ const Index = ({ body }: body) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const res = await axios.get(
-    `${process.env.API_BASE_LIST}?MobileOS=ETC&MobileApp=CAMP&_type=json&ServiceKey=eBL%2BCtX6h7sJAbi%2BnteZ72T3WWPGYuVXecg%2FbnWu34ifsQqa%2BuVRuZgdI9NVnGT83mAgY3gEj%2B8UJ7H9B1qTag%3D%3D`
+    `${process.env.API_BASE_LIST}?MobileOS=ETC&MobileApp=CAMP&_type=json&ServiceKey=${process.env.API_KEY}`
   );
   const {
     data: {
